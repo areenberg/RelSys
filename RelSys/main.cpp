c58412dc3df4ad32b9f0cde6db49f5c4789c91dc
Anders Reenberg Andersen <arean@dtu.dk>
2021-08-11 23:47:56 +0200

    Fixed a small index issue in example main-file

--- RelSys/main.cpp
+++ RelSys/main.cpp
@@ -1,5 +1,5 @@
 
-/* 
+/*
  * File:   main.cpp
  * Author: Anders Reenberg Andersen
  *
@@ -23,26 +23,26 @@ int main(int argc, char** argv) {
     //--------------------------
     //CUSTOMER SETUP
     //--------------------------
-    
+
     //total number of customer types
-    int nCustomerTypes = 4; 
+    int nCustomerTypes = 4;
 
     //arrival rates for each customer type
     vector<double> arrivalRates = {1.2,1.8,0.5,2.5};
-    
+
     //mean service time for each customer type
     vector<double> serviceTimes = {10,50,25,5};
-    
+
     //fraction of rejected customers that are moved to an alternative queue node
     //this is an nCustomerTypes x nQueues matrix
     vector<vector<double>> relProbs = {{0.0,0.4,0.1,0.5},
                                        {0.0,0.0,0.2,0.3}, //<<-- note: these do not have to sum to one
                                        {0.2,0.0,0.0,0.8},
                                        {0.9,0.05,0.05,0.0}};
-    
+
     //queue indices preferred by each customer type
     vector<int> preferredQueue = {0,1,2,3};
-    
+
     //create the customer type objects
     CustomerData * custs_array = new CustomerData[nCustomerTypes];
     for (int i=0; i<nCustomerTypes; i++){
@@ -51,53 +51,53 @@ int main(int argc, char** argv) {
                                       serviceTimes[i],
                                       relProbs[i]);
     }
-    
+
     //--------------------------
     //QUEUE SETUP
     //--------------------------
-    
-    //total number of queues    
-    int nQueues = 4; 
-    
+
+    //total number of queues
+    int nQueues = 4;
+
     //capacity of each queue
     vector<int> capacity = {15,18,12,16};
-    
+
     //calculate system input parameters from customer types to queues
     SystemParameters sysParam(nQueues,nCustomerTypes,custs_array);
-    
+
     //now create the queue objects
     QueueData * wd_array = new QueueData[nQueues];
     for (int i=0; i<nQueues; i++){
         wd_array[i] = QueueData(i,
                                 sysParam.queueArrivalRate(i),
                                 sysParam.queueServiceRate(i),
-                                capacity[0],
+                                capacity[i],
                                 sysParam.queueRelProbability(i));
     }
-    
+
     //--------------------------
     //HEURISTIC EVALUATION
     //--------------------------
-    
+
     //setup model object
     RelocEvaluation mdl(nQueues,wd_array);
-    
+
     //first simulate open/blocked time-windows
     int seed = 123;
     mdl.runSimulation(seed,365,365,50);
-    
+
     //choose a queue to evaluate
     int widx = 0; //queue index to be evaluated
     mdl.runHeuristic(widx);
-    
+
     //get the result
     cout << "--- RESULTS ---" << endl;
-   
+
     cout << "Marginal probability distribution:" << endl;
     for (int i=0; i<mdl.marginalDist.size(); i++){
         cout << mdl.marginalDist[i] << endl;
     }
-    
+
     cout << "Probability of rejection:" << endl;
     cout << mdl.blockingProbability << endl;
 
@@ -110,19 +110,19 @@ int main(int argc, char** argv) {
     //--------------------------
     //SIMULATION EVALUATION
     //--------------------------
-       
+
     //setup simulation model object
     RelocSimulation sim_mdl(nQueues,wd_array);
-        
+
     //setup and run simulation
     sim_mdl.setSeed(123); //set the seed
     double burnIn = 365; //burn-in time
     double minTime = 100000; //minimum simulation time
     vector<int> maxWardSamples(1,-1); //disables the limit on occupancy samples
     sim_mdl.disableTimeSampling(); //speed-up the simulation by disabling the open/blocked time-window sampling
-    
+
     sim_mdl.simulate(burnIn,minTime,maxWardSamples); //now run the simulation
-    
+
     //In contrary to the heuristic evaluation, the entire system is evaluated at the
     //same time. However, for the sake of this demonstration we choose only to print
     //the results from a single queue.
@@ -130,7 +130,7 @@ int main(int argc, char** argv) {
 
     //get the result
     cout << "--- RESULTS ---" << endl;
-   
+
     cout << "Marginal frequency distribution:" << endl;
     for (int i=0; i<sim_mdl.wardFreqDist[sim_widx].size(); i++){
         cout << sim_mdl.wardFreqDist[sim_widx][i] << endl;
@@ -139,7 +139,7 @@ int main(int argc, char** argv) {
     for (int i=0; i<sim_mdl.wardFreqDist[sim_widx].size(); i++){
         cout << sim_mdl.wardDenDist[sim_widx][i] << endl; //corresponds to marginalDist in the heuristic evaluation
     }
-    
+
     cout << "Probability of rejection:" << endl;
     cout << sim_mdl.blockingProbability[sim_widx] << endl;
 
@@ -148,7 +148,6 @@ int main(int argc, char** argv) {
 
     cout << "Expected fraction of servers occupied:" << endl;
     cout << sim_mdl.expOccFraction[sim_widx] << endl;
-        
+
     return 0;
 }
-
